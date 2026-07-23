044709a49fb137038f611de44bce9481a45baf8c
Pranayyy29 <pranayakole333@gmail.com>
2026-07-24 01:11:07 +0530

    Setup parser and syntax tree headers (#2)
    * Add AST statement definitions in stmt.h
    
    * modified token.h to make literals,lexeme,type public. added header files for parsing expressions
    
    * Rename parser methods and add line/column to Expr base class
    
    * Update stmt.h with line tracking and proper spacing
    
    * added left uniue pointer instead of token name
    
    * Fix AssignmentExpr member initialization
    
    * store line and column in statement nodes
    
    ---------
    
    Co-authored-by: kashyap <complexity266@gmail.com>

---
+++ include/parser.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <initializer_list>
+
+#include "token.h"
+#include "expr.h"
+#include "stmt.h"
+
+class Parser {
+public:
+    explicit Parser(std::vector<Token> tokens);
+    std::vector<std::unique_ptr<Stmt>> parse();
+
+private:
+    // Expressions
+    std::unique_ptr<Expr> parseExpression();
+    std::unique_ptr<Expr> parseAssignment();
+    std::unique_ptr<Expr> equality();
+    std::unique_ptr<Expr> comparison();
+    std::unique_ptr<Expr> term();
+    std::unique_ptr<Expr> factor();
+    std::unique_ptr<Expr> unary();
+    std::unique_ptr<Expr> primary();
+
+    // Statements
+    std::unique_ptr<Stmt> parseStatement();
+    std::unique_ptr<Stmt> parseExpressionStatement();
+    std::unique_ptr<Stmt> parseIfStatement();
+    std::unique_ptr<Stmt> parseWhileStatement();
+    std::unique_ptr<Stmt> parseReturnStatement();
+    std::unique_ptr<BlockStmt> parseBlock();
+
+    bool match(std::initializer_list<TokenType> types);
+    bool check(TokenType type) const;
+    Token advance();
+    bool isAtEnd() const;
+    Token peek() const;
+    Token previous() const;
+    Token consume(TokenType type, const std::string& message);
+
+    std::vector<Token> tokens_;
+    std::size_t current_ = 0;
+};
---
+++ include/stmt.h
@@ -0,0 +1,77 @@
+#pragma once 
+#include <memory>
+#include <vector>
+#include <utility>
+
+class Expr;
+
+class Stmt
+{
+    public:
+    Stmt(int line, int column)
+        : line(line), column(column) {}
+    virtual ~Stmt() = default;
+    int line;
+    int column;
+};
+
+class ExpressionStmt : public Stmt
+{
+    public:
+    std::unique_ptr<Expr> expression;
+
+    ExpressionStmt(std::unique_ptr<Expr> expr, int line, int column)
+        : Stmt(line, column),
+          expression(std::move(expr)) {}
+};
+
+class ReturnStmt : public Stmt
+{
+    public:
+    std::unique_ptr<Expr> value;
+    ReturnStmt(std::unique_ptr<Expr> val, int line, int column)
+        : Stmt(line, column),
+          value(std::move(val)) {}
+};
+
+class WhileStmt : public Stmt
+{
+public:
+    std::unique_ptr<Expr> condition;
+    std::unique_ptr<Stmt> body;
+    WhileStmt(std::unique_ptr<Expr> cond,
+              std::unique_ptr<Stmt> stmt,
+              int line,
+              int column)
+        : Stmt(line, column),
+          condition(std::move(cond)),
+          body(std::move(stmt)) {}
+};
+
+class IfStmt : public Stmt
+{
+public:
+    std::unique_ptr<Expr> condition;
+    std::unique_ptr<Stmt> thenBranch;
+    std::unique_ptr<Stmt> elseBranch;
+
+    IfStmt(std::unique_ptr<Expr> cond,
+           std::unique_ptr<Stmt> thenStmt,
+           std::unique_ptr<Stmt> elseStmt,
+           int line,
+           int column)
+        : Stmt(line, column),
+          condition(std::move(cond)),
+          thenBranch(std::move(thenStmt)),
+          elseBranch(std::move(elseStmt)) {}
+};
+
+class BlockStmt : public Stmt
+{
+public:
+    std::vector<std::unique_ptr<Stmt>> statements;
+
+    explicit BlockStmt(std::vector<std::unique_ptr<Stmt>> stmts, int line, int column)
+        : Stmt(line, column),
+          statements(std::move(stmts)) {}
+};
